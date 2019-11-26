7b4eafd43b4eb1ff7564cd6751e7b985bea868d1
andrei1902 <andrei.ionita.1902@gmail.com>
2019-11-26 15:28:24 +0200

    Added CreateUserEndPoint

--- src/github.com/andrei1902/rest-api-go/Main.go
+++ src/github.com/andrei1902/rest-api-go/Main.go
@@ -12,11 +12,22 @@ import (
 
 // User struct
 type User struct {
-	ID   primitive.ObjectID `json:"_id, omitempty" bson:"_id,omitempty"`
-	Name string             `bson:"Name,omitempty" bson: "Name,omitempty"`
+	Id   primitive.ObjectID `json:"_id, omitempty" bson:"_id,omitempty"`
+	Name string             `json:"name,omitempty" bson: "name,omitempty"`
+	Email string             `json:"email,omitempty" bson: "email,omitempty"`
 }
 
 var client *mongo.Client
+
+func CreateUserEndPoint(response http.ResponseWriter, request *http.ReadRequest) {
+	response.Header().Add("content-type", "application/json")
+	var user User
+	json.NewDecoder(request.Body).Decode(&user)
+	collection := client.Database("AndreiCluster").Collection("users")
+	ctx, _ := contex.WithTimeout(context.Background(), 10*time.Second)
+	result,  := collection.InsertOne(ctx, person)
+	json.NewEncoder(response).Encode(result)
+}
 
 func main() {
 	fmt.Println("Hello from GO!")
@@ -24,5 +35,6 @@ func main() {
 	client, + = mongo.Connect(ctx, "mongodb+srv://andrei123:Zsxo19qk7KUnEqwo@andreicluster-dkx3z.mongodb.net/test?retryWrites=true^&w=majority")
 
 	router := mux.NewRouter()
+	router.HandleFunc("/users", CreateUserEndPoint).Methods("POST")
 	http.ListenAndServe(":12345", router)
 }
