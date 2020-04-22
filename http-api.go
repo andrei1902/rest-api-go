f1bbaa4ca2ddd8543d22fd36ef203fac365b9464
andrei19021 <andrei.ionita.1902@gmail.com>
2020-04-22 19:47:59 +0300

    Adds delete user route

--- http-api.go
+++ http-api.go
@@ -26,13 +26,13 @@ func main() {
 	// get download/user?format="csv"
 	// get download/user?format="txt"
 	// get download/user?format="xml"
-	// delete /user/{id}
 	router.HandleFunc("/", test)
 	router.HandleFunc("/users", addUser).Methods("POST")
 	router.HandleFunc("/users", getUsers).Methods("GET")
 	router.HandleFunc("/users/{userID}", getUser).Methods("GET")
 	router.HandleFunc("/users/{userID}", updateUser).Methods("PUT")
 	router.HandleFunc("/users/{userID}", patchUser).Methods("PATCH")
+	router.HandleFunc("/users/{userID}", deleteUser).Methods("DELETE")
 	// logger for every request
 	// centralised error processing
 	fmt.Println("Server started...")
@@ -46,7 +46,7 @@ func main() {
 
 func test(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(CustomResponse{"Server is active..."})
+	w.Write([]byte("Server is active..."))
 }
 
 func addUser(w http.ResponseWriter, r *http.Request) {
@@ -123,4 +123,23 @@ func patchUser(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["userID"]
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("userID should be an integer, dumbass!"))
+		return
+	}
+	if id >= len(users) {
+		w.WriteHeader(404)
+		w.Write([]byte("User, not found, my killa!"))
+		return
+	}
+
+	users = append(users[:id], users[id+1:]...)
+
+	w.Write([]byte("The user was deleted"))
 }
